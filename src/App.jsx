b087d9052227574d15b03de61ac646bd69d25cae
NiharikaNeha <nehaniharikaswain07@gmail.com>
2025-11-02 21:28:19 +0530

    WishList Updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { animateScroll as scroll } from "react-scroll";
 import Index from "./components/Pages/Index.jsx";
 import ProductDetails from "./components/Pages/ProductDetails.jsx";
 import Wishlist from "./components/Pages/Wishlist.jsx";
+import Cart from "./components/Pages/Cart.jsx";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -31,6 +32,7 @@ const App = () => {
           <Route path="/" element={<Index />} />
           <Route path="/product/:id" element={<ProductDetails />} />
           <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
     </>
--- src/components/Pages/Cart.jsx
+++ src/components/Pages/Cart.jsx
@@ -1,35 +1,71 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { FaShoppingCart } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-  const [cart, setCart] = useState([])
-  const [couponCode, setCouponCode] = useState('')
-  const [discount, setDiscount] = useState(0)
+  const [cart, setCart] = useState([]);
+  const [couponCode, setCouponCode] = useState("");
+  const [discount, setDiscount] = useState(0);
 
   useEffect(() => {
-    const storedCart = JSON.parse(localStorage.getItem('cartItems')) || [];
-    const fixedCart = storedCart.map(item => ({
+    const storedCart = JSON.parse(localStorage.getItem("cartItems")) || [];
+    const fixedCart = storedCart.map((item) => ({
       ...item,
       Price: parseFloat(item.Price),
       quantity: item.quantity || 1,
-    }))
-    setCart(fixedCart)
-  }, [])
+    }));
+    setCart(fixedCart);
+  }, []);
 
   const updateQuantity = (id, delta) => {
-    const updatedCart = cart.map(item => {
-      if(item.Id === id) {
-        const newQty = Math.max(1, item.quantity + delta)
-        return {...item, quantity: newQty}
+    const updatedCart = cart.map((item) => {
+      if (item.Id === id) {
+        const newQty = Math.max(1, item.quantity + delta);
+        return { ...item, quantity: newQty };
       }
-      return item
-    })
-    setCart(updatedCart)
-    localStorage.setItem('cartItems',JSON.stringify(updatedCart))
-  }
-  return (
-    <div>Cart</div>
-  )
-}
+      return item;
+    });
+    setCart(updatedCart);
+    localStorage.setItem("cartItems", JSON.stringify(updatedCart));
+  };
+  const removeFromCart = (id) => {
+    const confirmRemove = window.confirm(
+      "Do You Want To Remove This Item From Cart?"
+    );
+    if (!confirmRemove) return;
 
-export default Cart
+    const updatedCart = cart.filter((item) => item.Id !== id);
+    setCart(updatedCart);
+    localStorage.setItem("cartItems", JSON.stringify(updatedCart));
+    toast.success("Item Removed From Cart");
+  };
+  const handleApplyCoupon = () => {
+    const code = couponCode.trim().toLowerCase();
+    if (code === "freeship10") {
+      setDiscount(10);
+      toast.success("10% Discount Applied");
+    } else {
+      setDiscount(0);
+      toast.error("Invalid Coupon Code");
+    }
+  };
+  const subtotal = cart.reduce((acc, item) => acc + item.Price * item.quantity);
+  const shipping = cart.length ? 300 : 0;
+  const discountAmount = (subtotal * discount) / 100;
+  const total = subtotal - discountAmount + shipping;
+  const navigate = useNavigate();
+  const handlePlaceOrder = () => {
+    navigate("/checkout");
+  };
+  return (
+    <>
+    <div className="px-4 sm:px-8 lg:px-[12%] py-12 bg-gray-800 min-h-screen">
+      <h1 className="text-2xl sm-text-3xl md:text-4xl font-bold text-center mb-12 text-yellow-600 font-bricolage"><FaShoppingCart />My SHopping Cart</h1>
+    </div>
+    </>
+  );
+};
+
+export default Cart;
