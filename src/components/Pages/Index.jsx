1b926d5b10adff86b0224d7f7453f794d02bace2
NiharikaNeha <nehaniharikaswain07@gmail.com>
2025-11-25 19:15:56 +0530

    updated

--- src/components/Pages/Index.jsx
+++ src/components/Pages/Index.jsx
@@ -53,7 +53,7 @@ const Index = () => {
 
   return (
     <>
-      <div className="bg-element"></div>
+      <div className="element"></div>
 
       {/* HERO SECTION */}
       <div className="hero-bg">
